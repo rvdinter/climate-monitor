10be3bff1790b4be884b0e387a5e969680063a78
Raymon van Dinter <raymon.van.dinter@sioux.eu>
2022-06-21 22:27:52 +0200

    updated graphics

--- ui/graphics.py
+++ ui/graphics.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageFont
 small_font = ImageFont.truetype(
     # "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
     r"C:\Windows\Fonts\arial.ttf",
-    16
+    12
 )
 medium_font = ImageFont.truetype(
     # "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
@@ -95,7 +95,6 @@ class Graphics:
     def set_pm_data(self, pm_data):
         self.pm_data = pm_data
 
-
     def set_bme_data(self, bme_data):
         self.bme_data = bme_data
 
@@ -104,7 +103,12 @@ class Graphics:
 
     def get_time(self):
         now = datetime.now()
-        self._time_text = now.strftime("%I:%M %p").lstrip("0").replace(" 0", " ")
+        if self.am_pm:
+            time_format = "%I:%M %p"
+        else:
+            time_format = "%H:%M"
+
+        self._time_text = now.strftime(time_format).lstrip("0").replace(" 0", " ")
 
     def update_display(self):
         # self.display.fill(Adafruit_EPD.WHITE)
@@ -115,56 +119,95 @@ class Graphics:
         (font_width, font_height) = icon_font.getsize(self._weather_icon)
         draw.text(
             (
-                self.display_width // 2 - font_width // 2,
-                self.display_height // 2 - font_height // 2 - 5,
+                self.display_width - font_width - 5,
+                0,
             ),
             self._weather_icon,
             font=icon_font,
             fill=BLACK,
         )
 
+        # Draw the outdoor temperature
+        (font_width, font_height) = large_font.getsize(self._temperature)
+        draw.text(
+            (
+                (self.display_width - font_width ) - font_width,
+                15,
+            ),
+            self._temperature,
+            font=self.large_font,
+            fill=BLACK,
+        )
+
         # Draw the city
         draw.text(
-            (5, 5), self._city_name, font=self.medium_font, fill=BLACK,
+            (5, 5), self._city_name, font=self.small_font, fill=BLACK,
         )
 
         # Draw the time
         (font_width, font_height) = medium_font.getsize(self._time_text)
         draw.text(
-            (5, font_height * 2 - 5),
+            (5, font_height ),
             self._time_text,
             font=self.medium_font,
             fill=BLACK,
         )
 
-        # Draw the main text
-        (font_width, font_height) = large_font.getsize(self._main_text)
-        draw.text(
-            (5, self.display_height - font_height * 2),
-            self._main_text,
-            font=self.large_font,
-            fill=BLACK,
-        )
+        # # Draw the main text
+        # (font_width, font_height) = large_font.getsize(self._main_text)
+        # draw.text(
+        #     (5, self.display_height - font_height * 2),
+        #     self._main_text,
+        #     font=self.large_font,
+        #     fill=BLACK,
+        # )
 
-        # Draw the description
-        (font_width, font_height) = small_font.getsize(self._description)
-        draw.text(
-            (5, self.display_height - font_height - 5),
-            self._description,
-            font=self.small_font,
-            fill=BLACK,
-        )
+        # # Draw the description
+        # (font_width, font_height) = small_font.getsize(self._description)
+        # draw.text(
+        #     (5, self.display_height - font_height - 5),
+        #     self._description,
+        #     font=self.small_font,
+        #     fill=BLACK,
+        # )
 
-        # Draw the temperature
-        (font_width, font_height) = large_font.getsize(self._temperature)
+        # Draw the indoor temperature
+        indoor_temp = f"{self.bme_data['temp']:.0f} °C"
+        (font_width, font_height) = large_font.getsize(indoor_temp)
         draw.text(
             (
                 self.display_width - font_width - 5,
                 self.display_height - font_height * 2,
             ),
-            self._temperature,
+            indoor_temp,
             font=self.large_font,
             fill=BLACK,
+        )
+
+        # Draw the AQI
+        aqi = f"AQI: {aqi_data:.0f}"
+        (font_width, font_height) = icon_font.getsize(aqi)
+        draw.text(
+            (
+                5,
+                self.display_height // 2 - font_height // 2,
+            ),
+            aqi,
+            font=large_font,
+            fill=BLACK,
+        )
+
+        # Draw the PM
+        draw.rectangle((0, self.display_height - 15, self.display_width, self.display_height), fill=BLACK)
+        pm_text = (f"PM 1.0: {pm_data['pm10 standard']}    "
+              f"PM2.5: {pm_data['pm25 standard']}    "
+              f"PM10: {pm_data['pm100 standard']}")
+        (font_width, font_height) = small_font.getsize(pm_text)
+        draw.text(
+            (5, self.display_height - font_height - 3),
+            pm_text,
+            font=self.small_font,
+            fill=WHITE,
         )
         return image
         # self.display.image(image)
@@ -186,7 +229,18 @@ if __name__ == '__main__':
                'sys': {'type': 2, 'id': 2037908, 'country': 'NL', 'sunrise': 1655781381, 'sunset': 1655841594},
                'timezone': 7200, 'id': 2759660, 'name': 'Gemeente Arnhem', 'cod': 200}
 
+    bme_data = {'temp': 20.15354,
+                'gas': 6846843,
+                'humidity': 40.325,
+                'pressure': 1068.35684,
+                'altitude': 10.358}
+    pm_data = {'pm10 standard': 15, 'pm25 standard': 20, 'pm100 standard': 800}
+    aqi_data = 20.32
+
     gfx = Graphics(None, am_pm=False, celsius=True)
+    gfx.set_pm_data(pm_data)
+    gfx.set_bme_data(bme_data)
+    gfx.set_aqi_data(aqi_data)
     gfx.set_weather(weather)
     gfx.get_time()
     img = gfx.update_display()
